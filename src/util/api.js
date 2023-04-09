c02f237d14c8f97f2760ec953144cd42ed48d76e
aemaaeng <haemin120@naver.com>
2023-04-09 18:57:57 +0900

    feat: 토큰 포함 axios Instance 생성

--- src/util/api.js
+++ src/util/api.js
@@ -3,11 +3,25 @@ import axios from "axios";
 const BASE_URL = "https://www.pre-onboarding-selection-task.shop/";
 
 // 인증이 필요없는 요청
-const axiosApi = (endpoint, options) => {
-  const instance = axios.create({ baseURL: endpoint, ...options });
+const axiosApi = (url, options) => {
+  const instance = axios.create({ baseURL: url, ...options });
   return instance;
 };
 
 // TODO: 인증이 필요한 요청
+const axiosAuthApi = (url, options) => {
+  const accessToken = localStorage.getItem("accessToken");
+  const instance = axios.create({
+    baseURL: url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+    ...options,
+  });
+
+  return instance;
+};
 
 export const defaultInstance = axiosApi(BASE_URL);
+export const authInstance = axiosAuthApi(BASE_URL);
