ca9bec4d1a75ea5192a479692cf17e315771c5db
aemaaeng <haemin120@naver.com>
2023-04-09 17:22:02 +0900

    feat: 로그인 여부에 따른 리다이렉트 처리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Auth from "./pages/Auth";
+import Todo from "./pages/Todo";
+import PrivateRoute from "./util/PrivateRoute";
+import AuthenticatedRoute from "./util/AuthenticatedRoute";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Auth pageTitle="회원가입" />} />
-        <Route path="/signin" element={<Auth pageTitle="로그인" />} />
-        <Route path="/todo" element={<Main />} />
+        <Route element={<AuthenticatedRoute />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<Auth pageTitle="회원가입" />} />
+          <Route path="/signin" element={<Auth pageTitle="로그인" />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/todo" element={<Todo />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
