4bef39e03015d7ac8d69b97f83480d06664fc117
aemaaeng <haemin120@naver.com>
2023-04-09 16:30:46 +0900

    feat: 로그인 폼 구현 및 API 연동

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Main />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/todo" element={<Main />} />
       </Routes>
     </BrowserRouter>
