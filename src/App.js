5527c605e80bd9bfc021209881811bcc9907877d
aemaaeng <haemin120@naver.com>
2023-04-10 19:03:42 +0900

    chore: SPA 배포 관련 파일 작업

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/wanted-pre-onboarding-frontend">
       <Routes>
         <Route element={<AuthenticatedRoute />}>
           <Route path="/" element={<Main />} />
